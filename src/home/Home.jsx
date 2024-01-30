c8dade877c68f8140f6b9ff1304124ee64d92353
Wendy-wyt <wuyuting0101@gmail.com>
2024-01-30 15:50:10 -0500

    completed navbar fixed at the top

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -3,7 +3,49 @@ import "./home.scss"
 
 const Home = () => {
     return (
-        <div className='home'><Navbar /></div>
+        <div className='home'>
+            <Navbar />
+            <img
+                width="100%"
+                src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                alt=""
+            />
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis repellat vel possimus illo veritatis eveniet. Doloribus id aliquid sunt, dignissimos iste hic modi explicabo ducimus officiis ex dolor alias vel.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum expedita facere, obcaecati sed libero, debitis ut ipsum harum placeat odit fugiat consectetur autem nostrum vitae distinctio praesentium ex sapiente repudiandae.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos eaque repudiandae officia voluptatibus nostrum sequi nam, beatae cumque amet, voluptatum ullam expedita tempore perferendis temporibus incidunt? Architecto, tempore nam! Architecto!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur molestiae quibusdam harum quam perferendis, aliquid recusandae voluptas possimus ad numquam incidunt ex quidem laudantium delectus omnis ipsa minus eligendi officia.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum dignissimos enim temporibus vel at odio magnam quisquam necessitatibus nam illum explicabo tempora, quia doloremque, omnis deleniti cupiditate praesentium. Quo, iure!
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis repellat vel possimus illo veritatis eveniet. Doloribus id aliquid sunt, dignissimos iste hic modi explicabo ducimus officiis ex dolor alias vel.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum expedita facere, obcaecati sed libero, debitis ut ipsum harum placeat odit fugiat consectetur autem nostrum vitae distinctio praesentium ex sapiente repudiandae.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos eaque repudiandae officia voluptatibus nostrum sequi nam, beatae cumque amet, voluptatum ullam expedita tempore perferendis temporibus incidunt? Architecto, tempore nam! Architecto!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur molestiae quibusdam harum quam perferendis, aliquid recusandae voluptas possimus ad numquam incidunt ex quidem laudantium delectus omnis ipsa minus eligendi officia.
+            Lore
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis repellat vel possimus illo veritatis eveniet. Doloribus id aliquid sunt, dignissimos iste hic modi explicabo ducimus officiis ex dolor alias vel.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum expedita facere, obcaecati sed libero, debitis ut ipsum harum placeat odit fugiat consectetur autem nostrum vitae distinctio praesentium ex sapiente repudiandae.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos eaque repudiandae officia voluptatibus nostrum sequi nam, beatae cumque amet, voluptatum ullam expedita tempore perferendis temporibus incidunt? Architecto, tempore nam! Architecto!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur molestiae quibusdam harum quam perferendis, aliquid recusandae voluptas possimus ad numquam incidunt ex quidem laudantium delectus omnis ipsa minus eligendi officia.
+            Lore
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis repellat vel possimus illo veritatis eveniet. Doloribus id aliquid sunt, dignissimos iste hic modi explicabo ducimus officiis ex dolor alias vel.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum expedita facere, obcaecati sed libero, debitis ut ipsum harum placeat odit fugiat consectetur autem nostrum vitae distinctio praesentium ex sapiente repudiandae.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos eaque repudiandae officia voluptatibus nostrum sequi nam, beatae cumque amet, voluptatum ullam expedita tempore perferendis temporibus incidunt? Architecto, tempore nam! Architecto!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur molestiae quibusdam harum quam perferendis, aliquid recusandae voluptas possimus ad numquam incidunt ex quidem laudantium delectus omnis ipsa minus eligendi officia.
+            Lore
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis repellat vel possimus illo veritatis eveniet. Doloribus id aliquid sunt, dignissimos iste hic modi explicabo ducimus officiis ex dolor alias vel.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum expedita facere, obcaecati sed libero, debitis ut ipsum harum placeat odit fugiat consectetur autem nostrum vitae distinctio praesentium ex sapiente repudiandae.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos eaque repudiandae officia voluptatibus nostrum sequi nam, beatae cumque amet, voluptatum ullam expedita tempore perferendis temporibus incidunt? Architecto, tempore nam! Architecto!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur molestiae quibusdam harum quam perferendis, aliquid recusandae voluptas possimus ad numquam incidunt ex quidem laudantium delectus omnis ipsa minus eligendi officia.
+            Lore
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis repellat vel possimus illo veritatis eveniet. Doloribus id aliquid sunt, dignissimos iste hic modi explicabo ducimus officiis ex dolor alias vel.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum expedita facere, obcaecati sed libero, debitis ut ipsum harum placeat odit fugiat consectetur autem nostrum vitae distinctio praesentium ex sapiente repudiandae.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos eaque repudiandae officia voluptatibus nostrum sequi nam, beatae cumque amet, voluptatum ullam expedita tempore perferendis temporibus incidunt? Architecto, tempore nam! Architecto!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur molestiae quibusdam harum quam perferendis, aliquid recusandae voluptas possimus ad numquam incidunt ex quidem laudantium delectus omnis ipsa minus eligendi officia.
+            Lore
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis repellat vel possimus illo veritatis eveniet. Doloribus id aliquid sunt, dignissimos iste hic modi explicabo ducimus officiis ex dolor alias vel.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum expedita facere, obcaecati sed libero, debitis ut ipsum harum placeat odit fugiat consectetur autem nostrum vitae distinctio praesentium ex sapiente repudiandae.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos eaque repudiandae officia voluptatibus nostrum sequi nam, beatae cumque amet, voluptatum ullam expedita tempore perferendis temporibus incidunt? Architecto, tempore nam! Architecto!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur molestiae quibusdam harum quam perferendis, aliquid recusandae voluptas possimus ad numquam incidunt ex quidem laudantium delectus omnis ipsa minus eligendi officia.
+            Lore
+        </div>
     )
 }
 
