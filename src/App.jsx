006773683ec2ae27626c05e8290038fa6414ac13
Wendy-wyt <wuyuting0101@gmail.com>
2024-01-31 13:52:35 -0500

    completed register and watch pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import "./app.scss"
-import Home from "./home/Home";
+// import Home from "./home/Home";
+// import Watch from "./pages/watch/Watch";
+import Register from "./pages/register/Register";
 
 const App = () => {
-  return <Home />;
+  // return <Home />;
+  // return <Watch />;
+
+  return <Register />
 };
 
 export default App;
